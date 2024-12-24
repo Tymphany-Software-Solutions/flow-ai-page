073885efcadd6f992bd0ec8b87c501703c5c1eee
Frankie <Frankie.Liu@Tymphany.com>
2024-12-25 03:50:56 +0800

    second comiit

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   devServer: {
-    proxy: {
+    proxy	:{
 		disableHostCheck: true,
       // 配置跨域
       '/api': {
         target: 'https://dev1.tymecho.com', // 目标接口的域名
         ws: false,
         changeOrigin: true, // 是否允许跨域
-        pathRewrite: {
-          '^/api': '' // 替换成空字符，去掉/api前缀
-        }
+		"pathRewrite": {
+		      "^/api": ""
+		}
       }
     }
   }
